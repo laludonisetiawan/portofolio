a2e68a41cb5dbce09d80592a6d1771bfae84b670
laludonisetiawan <laludonisetiawan@gmail.com>
2022-07-19 01:19:50 +0800

    add navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,19 @@
-//hambuuuuuuurger
+//navbar fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
 
+    if (window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fixed');
+    }else {
+        header.classList.remove('navbar-fixed');
+    }
+    
+};
 
+//hamburger
+const navMenu = document.querySelector('#nav-menu');
 
-const hamburger = document.querySelector('#hamburger');
-
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active')
-})
+hamburger.addEventListener('click', function () {
+    navMenu.classList.toggle('hidden');
+});
